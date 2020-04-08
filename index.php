61bdf42ca82c10455ce5ab6791ab5bae846f4148
grobblog <ilya_kalent@mail.ru>
2020-04-08 14:13:21 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,213 @@
+<?php require ''; ?>
+    <body class="home blog">
+
+        <!-- Preloader Gif -->
+        <table class="doc-loader">
+            <tbody>
+                <tr>
+                    <td>
+                        <img src="demo-images/hotpng.com.png" alt="Loading...">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <!-- Left Sidebar -->
+        <div id="sidebar" class="sidebar">
+            <div class="menu-left-part">
+                <div class="search-holder">
+                    <label>
+                        <input type="search" class="search-field" placeholder="Type here to search..." value="" name="s" title="Search for:">
+                    </label>
+                </div>
+                <div class="site-info-holder">    
+                    <h1 class="site-title">Осень</h1>
+                    <p class="site-description">
+                        	
+Хватит веселиться, хватит горевать
+Можно расходиться, можно забывать
+Кто бы что ни сделал, кем бы кто не стал
+Никто не проиграл.
+                    </p>s
+                </div>
+                <?php require 'leftsidebartml.php'; ?>
+    
+        <!-- Home Content -->
+        <div id="content" class="site-content">
+            <div id="blog-wrapper">
+                <div class="blog-holder center-relative">
+
+
+                    <article id="post-1" class="blog-item-holder featured-post">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <div class="cat-links">
+                                    <ul>
+                                        <li>
+                                            <a href="#"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="entry-date published">19 марта, 2020</div>
+                                <h2 class="entry-title">
+                                    Страница посвещенная дискографии группы Гражданская оборона.
+                                </h2>
+                                <div class="excerpt">
+                                    В период с 1985 года по 2007 было выпущено 26 альбома, 6 сборников, 35 концертных альбома.<a class="read-more" href="single.html"></a>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+                    <article id="post-2" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="1album.html">1. Поганая молодёжь</a>
+                                </h2>
+                                <div class="entry-date published">15 треков</div>
+                                <div class="entry-date published">1985 год</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+                    <article id="post-3" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="2album.html">2. Оптимизм</a>
+                                </h2>
+                                <div class="entry-date published">11 треков</div>
+                                <div class="entry-date published">1985 год</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+
+                    <article id="post-4" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="about.html">3.Игра в бисер перед свиньями</a>
+                                </h2>
+                                <div class="entry-date published">13 треков</div>
+                                <div class="entry-date published">1986 год</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+
+                    <article id="post-5" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="single.html">4.Мышеловка</a>
+                                </h2>
+                                <div class="entry-date published">15 треков</div>
+                                <div class="entry-date published">1987 год</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+
+                    <article id="post-6" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="single.html">5.Хорошо</a>
+                                </h2>
+                                <div class="entry-date published">15 треков</div>
+                                <div class="entry-date published">1987 год</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+                    
+                    <article id="post-7" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="single.html">6.Тоталитаризм</a>
+                                </h2>
+                                <div class="entry-date published">15 треков</div>
+                                <div class="entry-date published">1987 год</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+                    
+                    <article id="post-3" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="2album.html">7.Некрофилия</a>
+                                </h2>
+                                <div class="entry-date published">17 треков</div>
+                                <div class="entry-date published">1987 год</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+                    
+                    <article id="post-3" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="2album.html">8.Красный альбом</a>
+                                </h2>
+                                <div class="entry-date published">12 треков</div>
+                                <div class="entry-date published">1987 год</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+                    
+                    <article id="post-3" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="2album.html">9.Все идет по плану</a>
+                                </h2>
+                                <div class="entry-date published">18 треков</div>
+                                <div class="entry-date published">1988 год</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+                </div>
+                <div class="clear"></div>
+                <div class="block load-more-holder">LOAD MORE ENTRIES</div>
+            </div>
+
+            <div class="featured-image-holder">
+                <div class="featured-post-image" style="background-image: url(demo-images/featured-image.jpg)"></div>
+
+            </div>
+            <div class="clear"></div>
+        </div>
+
+
+
+        <!--Load JavaScript-->
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type='text/javascript' src='js/imagesloaded.pkgd.js'></script>
+        <script type='text/javascript' src='js/jquery.nicescroll.min.js'></script>
+        <script type='text/javascript' src='js/jquery.smartmenus.min.js'></script>
+        <script type='text/javascript' src='js/jquery.carouFredSel-6.0.0-packed.js'></script>
+        <script type='text/javascript' src='js/jquery.mousewheel.min.js'></script>
+        <script type='text/javascript' src='js/jquery.touchSwipe.min.js'></script>
+        <script type='text/javascript' src='js/jquery.easing.1.3.js'></script>
+        <script type='text/javascript' src='js/main.js'></script>
+    </body>
+</html>
